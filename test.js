74086bfd0a592bf75add95d20fe7d9740791641e
Agustin <aguscpl10@gmail.com>
2023-12-09 11:40:20 -0300

    Se realizan correciones varias, se agregan alias para mejor manejo de archivos

--- test.js
+++ test.js
@@ -2,15 +2,31 @@ import axios from 'axios';
 import fs from 'fs/promises';
 import moment from 'moment';
 
-// URL de la página de descargas
-const urlsProveedores = 
-['https://www.egsa.com.ar/descargas/egsalista1.xls',
-'https://www.egsa.com.ar/descargas/egsalista2.xls',
-'https://mm.sistorga.com.ar/web/ofertaFile/MovimientoMecanico/lista de precios movmec 05-12-2023.xls',
-'https://etman.com.ar/micuenta/generador_listas/5615.csv',
-'https://www.carlosvazquez.net/es/clientes/gen_xls_linea.php?numero_linea=TODAS&numero_cliente=37564']
-const fechaActual = moment().format('DDMMYY');
-console.log(fechaActual)
+const dynamicDate = moment().format('DD-MM-YY');
+
+const proveedores = [
+  {
+    url: 'https://www.egsa.com.ar/descargas/egsalista1.xls',
+    alias: 'Goyanarte 1'
+  },
+  {
+    url: 'https://www.egsa.com.ar/descargas/egsalista2.xls',
+    alias: 'Goyanarte 2'
+  },
+  {
+    url: `https://mm.sistorga.com.ar/web/ofertaFile/MovimientoMecanico/lista de precios movmec ${dynamicDate}.xls`,
+    alias: 'movmec'
+  },
+  {
+    url: 'https://etman.com.ar/micuenta/generador_listas/5615.csv',
+    alias: 'etman'
+  },
+  {
+    url: 'https://www.carlosvazquez.net/es/clientes/gen_xls_linea.php?numero_linea=TODAS&numero_cliente=37564',
+    alias: 'carlosvazquez'
+  }
+];
+
 // Directorio donde se guardarán los archivos descargados
 const directorioDescargas = 'directorio_descargas';
 
@@ -19,21 +35,35 @@ async function descargarArchivo() {
   try {
     // Crear el directorio de descargas si no existe
     await fs.mkdir(directorioDescargas, { recursive: true });
-    
-    for (const urlProveedor of urlsProveedores) {
-      // Obtener el nombre del archivo desde la URL
-      const nombreArchivo = fechaActual + " " + urlProveedor.split('/').pop();
 
-      // Realizar la solicitud HTTP para descargar el archivo
-      const response = await axios.get(urlProveedor, { responseType: 'arraybuffer' });
+    for (const proveedor of proveedores) {
+      try {
+        const { url, alias } = proveedor;
 
-      // Guardar el archivo en el directorio de descargas
-      await fs.writeFile(`${directorioDescargas}/${nombreArchivo}`, response.data);
+        // Obtener el nombre del archivo desde el mapeo de alias
+        let nombreArchivo = `${dynamicDate} ${alias}.xls`;
 
-      console.log(`Archivo ${nombreArchivo} descargado desde ${urlProveedor}`);
+        // Verificar que el archivo tenga la extensión ".xls"
+        if (!nombreArchivo.toLowerCase().endsWith('.xls')) {
+          console.log(`El archivo ${nombreArchivo} no tiene la extensión ".xls". Cambiando la extensión a ".xls".`);
+
+          // Cambiar la extensión del archivo a ".xls"
+          nombreArchivo += '.xls';
+        }
+
+        // Realizar la solicitud HTTP para descargar el archivo
+        const response = await axios.get(url, { responseType: 'arraybuffer' });
+
+        // Guardar el archivo en el directorio de descargas
+        await fs.writeFile(`${directorioDescargas}/${nombreArchivo}`, response.data);
+
+        console.log(`Archivo ${nombreArchivo} descargado desde ${url}`);
+      } catch (error) {
+        console.error(`Error al descargar archivo desde ${proveedor.url}: ${error.message}`);
+      }
     }
   } catch (error) {
-    console.error(`Error al descargar archivo desde la lista de proveedores : ${error.message}`);
+    console.error(`Error al descargar archivos: ${error.message}`);
   }
 }
 
